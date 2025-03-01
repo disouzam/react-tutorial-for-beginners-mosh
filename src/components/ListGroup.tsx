a5cbbd52169f7c269f228f797b78436d3727aeab
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-01 16:04:44 -0300

    Use of a arrow function instead of a constant to get a message

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,12 +3,14 @@ function ListGroup() {
 
   items = [];
 
-  const message = items.length === 0 ? <p>No item found</p> : null;
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
 
   return (
     <>
       <h1>List</h1>
-      {message}
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
