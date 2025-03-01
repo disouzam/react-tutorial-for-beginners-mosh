5b361c80c40c78d362e226ee1545e729b2912bce
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-01 16:01:15 -0300

    Use of a constant to replace in-jsx conditionals

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,10 +3,12 @@ function ListGroup() {
 
   items = [];
 
+  const message = items.length === 0 ? <p>No item found</p> : null;
+
   return (
     <>
       <h1>List</h1>
-      {items.length === 0 ? <p>No item found</p> : null}
+      {message}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
