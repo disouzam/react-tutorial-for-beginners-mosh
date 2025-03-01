a4e1197cc85c67ec11fd21b4d4c0ff24938750e6
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-01 16:12:01 -0300

    Added a simple click event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,17 @@
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  // items = [];
-
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className="list-group-item"
+            onClick={(event) => console.log(event)}
+          >
             {item}
           </li>
         ))}
