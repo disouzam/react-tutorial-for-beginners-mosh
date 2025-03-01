0eadb6123976793020b875397af6fee61ee5ea70
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-01 16:07:26 -0300

    Final implementation of conditional display of a paragraph with a message when items has no items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,12 @@
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  items = [];
-
-  const getMessage = () => {
-    return items.length === 0 ? <p>No item found</p> : null;
-  };
+  // items = [];
 
   return (
     <>
       <h1>List</h1>
-      {getMessage()}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
