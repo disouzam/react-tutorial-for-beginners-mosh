e118f34f3cfe35ca4e2e23cfa7fd2bb9374ce2f6
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-01 15:55:09 -0300

    Extended ESLint rules to enformce brackets and also to get rid of outdated warnings of react in jsx scope

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,39 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import pluginReact, { rules } from "eslint-plugin-react";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+  },
+  {
+    languageOptions:
+    {
+      globals: globals.browser
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules:
+    {
+      curly: ["error", "all"],
+    },
+    plugins: ["react"],
+    settings:
+    {
+      react:
+      {
+        version: "detect",
+      },
+      rules:
+      {
+        "react/react-in-jsx-scope": "off",
+      },
+    }
+  }
 ];
